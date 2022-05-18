cb8c0be25c017b5d68890a9ec026f6ddd0b0a154
Martin Holub <mataholl@gmail.com>
2022-05-18 13:02:02 +0200

    js

---
+++ js-zaklady/prvni-appka/app.js
@@ -0,0 +1,17 @@
+console.log(__filename);
+console.log(__dirname);
+function rekniAhoj(name){
+    console.log('Ahoj' + name);
+}
+rekniAhoj('Martine');
+
+
+//window.console.log = console.log();
+
+var sayHello = function(){
+
+}
+var log = require('./logger');
+console.log(log);
+
+log('message');
